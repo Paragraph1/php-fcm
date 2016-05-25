624a0a4ec209ebac087b8b0ecf58af380147d11c
Kai Bielenberg <kai.bielenberg@gmail.com>
2016-05-25 12:14:12 +0200

    Added getter/setter

--- src/Notification.php
+++ src/Notification.php
@@ -3,8 +3,53 @@ namespace phpFCM;
 
 class Notification
 {
+	private $title;
+	private $body;
+	private $badge;
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBadge($badge)
+    {
+        $this->badge = $badge;
+        return $this;
+    }
+
+    public function getBadge()
+    {
+        return $this->badge;
+    }
+
     public function toJson()
     {
-        return json_encode($this);
+    	$json = array(
+    		'title' => $this->getTitle(),
+    		'body' => $this->getBody()
+    	);
+    	if ($this->getBadge()) {
+    		$json['badge'] = $this->getBadge();
+    	}
+
+        return json_encode($json);
     }
 }
