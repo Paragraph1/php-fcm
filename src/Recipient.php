b13de8cf145c527a1c52aec29859feee9f06a8a0
Kai Bielenberg <kai.bielenberg@gmail.com>
2016-05-25 12:31:27 +0200

    added setter to message

--- src/Recipient.php
+++ src/Recipient.php
@@ -3,5 +3,16 @@ namespace paragraph1\phpFCM;
 
 class Recipient
 {
+    private $to;
 
+    public function setTo($to)
+    {
+        $this->to = $to;
+        return this;
+    }
+
+    public function toJson()
+    {
+        return $to;
+    }
 }
